0200e0c56513002baefe9ee289a31435d5571efc
Ethan Dawn <yd041003@gmail.com>
2025-05-03 21:21:15 +0800

    2025_05_03 workstation_3

--- leetcode/2485.找出中枢整数.c
+++ leetcode/2485.找出中枢整数.c
@@ -6,7 +6,21 @@
 
 // @lc code=start
 int pivotInteger(int n) {
-    
+    int SUM = n * (n + 1) / 2, i;
+    int arr[1005], sum[1005];
+
+    sum[0] = 0;
+    for(i = 1; i <= n; i++) {
+        arr[i] = i;
+        sum[i] = sum[i - 1] + arr[i];
+    }
+
+    for(i = 1; i <= n; i++) {
+        if(sum[i] == SUM - sum[i - 1]) {
+            return i;
+        }
+    }
+    return -1;
 }
 // @lc code=end
 
