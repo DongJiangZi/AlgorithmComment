e847ad9e5246d5ed6d6b1d173e936c305c2dd0bd
ethan dawn <yd041003@gmail.com>
2025-05-06 09:56:06 +0800

    2025_05_06 lecture

--- leetcode/209.长度最小的子数组.c
+++ leetcode/209.长度最小的子数组.c
@@ -6,7 +6,26 @@
 
 // @lc code=start
 int minSubArrayLen(int target, int* nums, int numsSize) {
+    int ans = 1e5 + 1, l = 0, r = 0;
+    long sum = 0;
     
+    while(r <= numsSize - 1) {
+        sum += nums[r];
+        if(sum >= target) {
+            while(l <= r && sum - nums[l] >= target) {
+                sum -= nums[l];
+                l++;
+            }
+            if(r - l + 1 < ans) {
+                ans = r - l + 1;
+            }
+        }
+        r++;
+    }
+    if(ans >= 1e5 + 1) {
+        ans = 0;
+    }
+    return ans;
 }
 // @lc code=end
 
